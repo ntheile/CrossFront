263ae768f12bcb4bdfdb7510970e6f2130d4c637
unknown <u402046@.dow.com>
2012-10-19 16:32:20 -0400

    deploy to appharbor

--- CrossFront/js/views/pages/One.js
+++ CrossFront/js/views/pages/One.js
@@ -3,31 +3,29 @@ var __extends = this.__extends || function (d, b) {
     __.prototype = b.prototype;
     d.prototype = new __();
 }
-define(["require", "exports"], function(require, exports) {
-    var PageOne = (function (_super) {
-        __extends(PageOne, _super);
-        function PageOne(el, collection, options) {
-                _super.call(this);
-            this.el = el;
-        }
-        PageOne.prototype.initialize = function () {
-            console.log("Page one view init'd");
-        };
-        PageOne.prototype.render = function () {
-            $el = this.el;
-            var tmpl = require([
-                "text!../../../templates/pages/One.html!strip"
-            ], function (html) {
-                var compiled_template = _.template(html);
-                $el.html(compiled_template()).trigger('create');
-                return this;
-            });
-            return tmpl;
-            console.log("completed page one render");
-        };
-        return PageOne;
-    })(Backbone.View);
-    exports.PageOne = PageOne;    
-    ; ;
-})
+var PageOne = (function (_super) {
+    __extends(PageOne, _super);
+    function PageOne(el, collection, options) {
+        _super.call(this);
+        this.el = el;
+    }
+    PageOne.prototype.initialize = function () {
+        console.log("Page one view init'd");
+    };
+    PageOne.prototype.render = function () {
+        $el = this.el;
+        var tmpl = require([
+            "text!../../../templates/pages/One.html!strip"
+        ], function (html) {
+            var compiled_template = _.template(html);
+            $el.html(compiled_template()).trigger('create');
+            return this;
+        });
+        return tmpl;
+        console.log("completed page one render");
+    };
+    return PageOne;
+})(Backbone.View);
+exports.PageOne = PageOne;
+; ;
 
